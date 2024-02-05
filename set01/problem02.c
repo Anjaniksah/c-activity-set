89199da89f63c6f54fc1ce313e52245bac355769
Anjaniksah <141307447+Anjaniksah@users.noreply.github.com>
2024-02-04 21:02:40 -0800

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a,b,sum;
-    printf("enter the numbers:"\n);
+    printf("enter the numbers:\n");
     scanf("%d %d",&a,&b);
     sum=a+b;
     printf("The sum of two numbers is %d:",sum);
